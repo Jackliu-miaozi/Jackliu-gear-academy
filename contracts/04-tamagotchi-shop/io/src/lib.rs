1b1d278a0e413baa53aec60e4855bf377775e24b
Jackliu-miaozi <71253778+Jackliu-miaozi@users.noreply.github.com>
2023-12-14 14:36:26 +0800

    tamagotchi_shop

--- contracts/04-tamagotchi-shop/io/src/lib.rs
+++ contracts/04-tamagotchi-shop/io/src/lib.rs
@@ -1,13 +1,24 @@
 #![no_std]
 
-use gmeta::Metadata;
-use gstd::prelude::*;
+use codec::{Decode, Encode};
+use gmeta::{In, InOut, Metadata, Out};
+use gstd::{prelude::*, ActorId};
 
 #[derive(Default, Encode, Decode, TypeInfo)]
 #[codec(crate = gstd::codec)]
 #[scale_info(crate = gstd::scale_info)]
 pub struct Tamagotchi {
     // TODO: 0️⃣ Copy fields from previous lesson and push changes to the master branch
+    pub name: String,
+    pub date_of_birth: u64,
+    pub owner: ActorId,
+    pub fed: u64,
+    pub fed_block: u64,
+    pub entertained: u64,
+    pub entertained_block: u64,
+    pub slept: u64,
+    pub slept_block: u64,
+    pub approved_account: Option<ActorId>,
     // TODO: 2️⃣ Add new fields
 }
 
@@ -16,6 +27,14 @@ pub struct Tamagotchi {
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgAction {
     // TODO: 0️⃣ Copy actions from previous lesson and push changes to the master branch
+    Name,
+    Age,
+    Feed,
+    Entertain,
+    Sleep,
+    Transfer(ActorId),
+    Approve(ActorId),
+    RevokeApproval,
     // TODO: 3️⃣ Add new actions
 }
 
@@ -24,6 +43,14 @@ pub enum TmgAction {
 #[scale_info(crate = gstd::scale_info)]
 pub enum TmgEvent {
     // TODO: 0️⃣ Copy events from previous lesson and push changes to the master branch
+    Name(String),
+    Age(u64),
+    Fed,
+    Entertained,
+    Slept,
+    Transferred(ActorId),
+    Approved(ActorId),
+    ApprovalRevoked,
     // TODO: 4️⃣ Add new events
 }
 
@@ -31,10 +58,18 @@ pub struct ProgramMetadata;
 
 // TODO: 0️⃣ Copy `Metadata` from the first lesson and push changes to the master branch
 impl Metadata for ProgramMetadata {
-    type Init = ();
-    type Handle = ();
-    type State = ();
+    type Init = In<String>;
+    type Handle = InOut<TmgAction, TmgEvent>;
+    type State = Out<Tamagotchi>;
     type Reply = ();
     type Others = ();
     type Signal = ();
 }
+
+pub const HUNGER_PER_BLOCK: u64 = 1;
+pub const BOREDOM_PER_BLOCK: u64 = 2;
+pub const ENERGY_PER_BLOCK: u64 = 2;
+
+pub const FILL_PER_FEED: u64 = 1000;
+pub const FILL_PER_ENTERTAINMENT: u64 = 1000;
+pub const FILL_PER_SLEEP: u64 = 1000;
