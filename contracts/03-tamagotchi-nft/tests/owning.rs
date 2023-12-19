253b305148ee7ec4169fbf5b7585596630a4feec
Jackliu-miaozi <71253778+Jackliu-miaozi@users.noreply.github.com>
2023-12-19 12:39:32 +0800

    tamagotchi_nft

--- contracts/03-tamagotchi-nft/tests/owning.rs
+++ contracts/03-tamagotchi-nft/tests/owning.rs
@@ -9,25 +9,26 @@ fn owning_test() {
     sys.init_logger();
     let program = Program::current(&sys);
     let _result = program.send(2, String::from("Goodmoring"));
-    let result = program.send(2, TmgAction::Transfer(1.into()));
-    let log = Log::builder()
-        .dest(2)
-        .payload(TmgEvent::Transferred(1.into()));
-    assert!(result.contains(&log));
-
-    let result = program.send(1, TmgAction::Approve(2.into()));
-    let log = Log::builder().dest(1).payload(TmgEvent::Approved(2.into()));
-    assert!(result.contains(&log));
-
     let result = program.send(2, TmgAction::Transfer(3.into()));
     let log = Log::builder()
         .dest(2)
         .payload(TmgEvent::Transferred(3.into()));
     assert!(result.contains(&log));
 
-    // let result = program.send(3, TmgAction::RevokeApproval);
-    // let log = Log::builder().dest(3).payload(TmgEvent::ApprovalRevoked);
-    // assert!(result.contains(&log));
+    let result = program.send(3, TmgAction::Approve(4.into()));
+    let log = Log::builder().dest(3).payload(TmgEvent::Approved(4.into()));
+    assert!(result.contains(&log));
+
+    let result = program.send(4, TmgAction::Transfer(5.into()));
+    let log = Log::builder()
+        .dest(4)
+        .payload(TmgEvent::Transferred(5.into()));
+    assert!(result.contains(&log));
+
+    let result = program.send(5, TmgAction::RevokeApproval);
+    let log = Log::builder().dest(5).payload(TmgEvent::ApprovalRevoked);
+    assert!(result.contains(&log));
+
     //why the test is panic?
     // TODO: 6️⃣ Test new functionality
 }
